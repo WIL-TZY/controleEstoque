f098369503d876c679452819ac8b0fc51336bc3b
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-10-26 22:55:40 +0000

    Joao updates

--- lib/index.dart
+++ lib/index.dart
@@ -1,2 +1,4 @@
 // Export pages
 export '/pages/home_page/home_page_widget.dart' show HomePageWidget;
+export '/cadastro/cadastro_widget.dart' show CadastroWidget;
+export '/login/login_widget.dart' show LoginWidget;
